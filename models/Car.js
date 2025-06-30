e65bdb585446df6b770c3558e923667e47e59284
sarabjitsinha <sinhasarabjit@gmail.com>
2025-06-30 08:56:34 +0530

    changed the cars file

--- models/Car.js
+++ models/Car.js
@@ -1,12 +1,25 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
+
+// const carSchema = new mongoose.Schema({
+//   name: String,
+//   year: Number,
+//   slug: { type: String, unique: true },
+//   image: String,
+//   description: String,
+//   price: Number
+// }, { timestamps: true });
+
+// export default mongoose.model('Car', carSchema);
+
+import mongoose from "mongoose";
 
 const carSchema = new mongoose.Schema({
-  name: String,
-  year: Number,
-  slug: { type: String, unique: true },
-  image: String,
-  description: String,
-  price: Number
+  name: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  year: { type: Number, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: false }, // URL to uploaded image
 }, { timestamps: true });
 
-export default mongoose.model('Car', carSchema);
+const Car = mongoose.model("Car", carSchema);
